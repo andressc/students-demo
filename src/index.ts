8ece46d937377165e56052ac553d5026a22e90f6
andrejrebrov <674511674511aaqqww>
2022-08-16 01:30:44 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3000
+
+const videos = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
+
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello World!';
+    res.send(helloMessage)
+})
+
+app.get('/videos/', (req: Request, res: Response) => {
+    res.send(videos)
+})
+
+app.get('/videos/:videoId', (req: Request, res: Response) => {
+    const id = +req.params.videoId;
+    const video = videos.filter(v => v.id === id)
+
+    if(!video) {
+        return "rfgr"
+    }
+
+    res.send(video)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
