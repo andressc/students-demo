aa3c43858d8e4d018e7f380463cd8fe09130f447
andrejrebrov <674511674511aaqqww>
2022-08-19 21:42:50 +0300

    lesson 1 fix test 1

--- src/repositories/videos-repository.ts
+++ src/repositories/videos-repository.ts
@@ -1,9 +1,35 @@
 let videos = [
-    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
-    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
-    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
-    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
-    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+    {
+        id: 1,
+        title: 'About JS - 01',
+        author: 'it-incubator.eu',
+        canBeDownloaded: true,
+        minAgeRestriction: 2,
+        availableResolutions: ["P144","Invalid","P720"],
+        createdAt: "2022-06-19T18:33:52.645Z",
+        publicationDate: "2022-08-19T18:33:52.645Z"
+    },
+    {
+        id: 2,
+        title: 'About JS - 02',
+        author: 'it-incubator.eu',
+        canBeDownloaded: true,
+        minAgeRestriction: 34,
+        availableResolutions: ["P144","Invalid","P720"],
+        createdAt: "2022-07-19T18:33:52.645Z",
+        publicationDate: "2022-08-19T18:33:52.645Z"
+    },
+    {
+        id: 3,
+        title: 'About JS - 03',
+        author: 'it-incubator.eu',
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        availableResolutions: ["P144","Invalid","P720"],
+        createdAt: "2022-09-19T18:33:52.645Z",
+        publicationDate: "2022-08-19T18:33:52.645Z"
+    },
+
 ];
 
 export const videosRepository = {
@@ -31,22 +57,50 @@ export const videosRepository = {
         return true;
     },
 
-    updateVideo(id: number, title: string, author: string = "it-incubator.eu") {
+    updateVideo(
+        id: number,
+        title: string,
+        author: string,
+        canBeDownloaded: boolean = false,
+        minAgeRestriction: number | null = null,
+        availableResolutions: [],
+        createdAt: string = new Date().toISOString(),
+        publicationDate: string = new Date().toISOString(),
+    ) {
         const video = videos.find(v => v.id === id);
         if(video) {
             video.title = title;
             video.author = author;
+            video.canBeDownloaded = canBeDownloaded;
+            video.minAgeRestriction = minAgeRestriction;
+            video.availableResolutions = availableResolutions;
+            video.createdAt = createdAt;
+            video.publicationDate = publicationDate;
+
             return true;
         }
 
         return false;
     },
 
-    createVideo(title: string, author: string = "it-incubator.eu") {
+    createVideo(
+        title: string,
+        author: string,
+        canBeDownloaded: boolean = true,
+        minAgeRestriction: number | null = null,
+        availableResolutions: [],
+        createdAt: string = new Date().toISOString(),
+        publicationDate: string = new Date().toISOString(),
+    ) {
         const newVideo = {
             id: +(new Date()),
             title,
-            author
+            author,
+            canBeDownloaded,
+            minAgeRestriction,
+            createdAt,
+            publicationDate,
+            availableResolutions
         };
         videos.push(newVideo);
         return newVideo.id;
--- src/routes/videos-router.ts
+++ src/routes/videos-router.ts
@@ -5,6 +5,10 @@ import {inputValidationMiddleware} from "../middlewares/input-validation-middlew
 
 export const videosRouter = Router({});
 
+enum resolutions {
+"P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"
+}
+
 const titleValidation = body('title')
     .isLength({max: 40})
     .withMessage("maximum 40 characters")
@@ -14,6 +18,21 @@ const titleValidation = body('title')
 const authorValidation = body('author')
     .isLength({max: 20})
     .withMessage("maximum 20 characters")
+    .notEmpty()
+    .withMessage("must not be empty");
+
+const availableResolutionsValidation = body("availableResolutions").isArray()
+    .isIn(["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"])
+    .withMessage("wrong resolution");
+
+const minAgeRestrictionValidation = body("minAgeRestriction")
+    .isLength({min: 1, max: 18})
+    .withMessage("minimum 1, maximum 18 characters");
+
+const canBeDownloadedValidation = body("canBeDownloaded")
+    .isBoolean()
+    .withMessage("must be boolean");
+
 
 
 videosRouter.get('/', (req: Request, res: Response) => {
@@ -48,9 +67,18 @@ videosRouter.delete('/:id', (req: Request, res: Response) => {
 videosRouter.post('/',
     titleValidation,
     authorValidation,
+    //availableResolutionsValidation,
+    minAgeRestrictionValidation,
+    canBeDownloadedValidation,
     inputValidationMiddleware,
     (req: Request, res: Response) => {
-    const newVideoId = videosRepository.createVideo(req.body.title, req.body.author);
+    const newVideoId = videosRepository.createVideo(
+        req.body.title,
+        req.body.author,
+        req.body.canBeDownloaded,
+        req.body.minAgeRestriction,
+        req.body.availableResolutions
+    );
     const [testNewVideo] = videosRepository.findVideoById(newVideoId);
     if(testNewVideo) {
         res.status(201).send(testNewVideo);
@@ -66,9 +94,20 @@ videosRouter.put('/', (req: Request, res: Response) => {
 videosRouter.put('/:id',
     titleValidation,
     authorValidation,
+    //availableResolutionsValidation,
+    minAgeRestrictionValidation,
+    canBeDownloadedValidation,
     inputValidationMiddleware,
     (req: Request, res: Response) => {
-    const isUpdated = videosRepository.updateVideo(+req.params.id, req.body.title, req.body.author);
+    const isUpdated = videosRepository.updateVideo(
+        +req.params.id,
+        req.body.title,
+        req.body.author,
+        req.body.canBeDownloaded,
+        req.body.minAgeRestriction,
+        req.body.availableResolutions,
+        req.body.publicationDate
+    );
     if(isUpdated) {
         res.send(204);
         return;
